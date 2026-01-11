5ae264ef7101600022b1cd085fed7b044f9f3108
MatiasS14 <matias.solanes14@gmail.com>
2026-01-11 18:51:46 -0300

    Se agrega validaciones

--- backend/churnInsight/src/main/java/com/churnInsight/churnInsight/domain/dto/UsuarioDTO.java
+++ backend/churnInsight/src/main/java/com/churnInsight/churnInsight/domain/dto/UsuarioDTO.java
@@ -1,5 +1,6 @@
 package com.churnInsight.churnInsight.domain.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,10 @@ import lombok.NoArgsConstructor;
 public class UsuarioDTO {
 
     private Long id;
+    @NotBlank
     private String usuario;
+    @NotBlank
     private String email;
+    @NotBlank
     private String password;
 }
