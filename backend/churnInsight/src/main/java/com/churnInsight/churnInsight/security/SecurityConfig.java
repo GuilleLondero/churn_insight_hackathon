5dba374e5837512bc3515ced106e191a0dd55991
MatiasS14 <matias.solanes14@gmail.com>
2026-01-24 21:52:54 -0300

    Permiso para acceder sin autenticacion a health agregado

--- backend/churnInsight/src/main/java/com/churnInsight/churnInsight/security/SecurityConfig.java
+++ backend/churnInsight/src/main/java/com/churnInsight/churnInsight/security/SecurityConfig.java
@@ -45,6 +45,7 @@ public class SecurityConfig {
         // 2. Configuración de Rutas
         http.authorizeHttpRequests(auth -> auth
         .requestMatchers(HttpMethod.OPTIONS,"/**").permitAll() //permite peticiones con headers
+        .requestMatchers("/health").permitAll()
         .requestMatchers("/auth/**").permitAll() // Login y Registro público
         .requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll() // Documentación
         
@@ -93,7 +94,7 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-        @Bean
+    @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
