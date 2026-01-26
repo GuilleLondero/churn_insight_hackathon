76e19434f9b963481f10f36191504989a862ba26
MatiasS14 <matias.solanes14@gmail.com>
2026-01-26 01:11:14 -0300

    Agregado enpoint vacio a config cors

--- backend/churnInsight/src/main/java/com/churnInsight/churnInsight/security/SecurityConfig.java
+++ backend/churnInsight/src/main/java/com/churnInsight/churnInsight/security/SecurityConfig.java
@@ -45,6 +45,7 @@ public class SecurityConfig {
         // 2. Configuración de Rutas
         http.authorizeHttpRequests(auth -> auth
         .requestMatchers(HttpMethod.OPTIONS,"/**").permitAll() //permite peticiones con headers
+        .requestMatchers("/").permitAll()
         .requestMatchers("/health").permitAll()
         .requestMatchers("/auth/**").permitAll() // Login y Registro público
         .requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll() // Documentación
